f68049697780bacb9502ac25f92b7b43c0520d2c
zss1q <apple@jd505-17.local>
2019-10-14 11:11:47 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,7 +1,7 @@
 //9x9的空表，请自行编写这段代码
 
 
-//倒九九乘法表
+//倒九九乘法空表
 str = '<table border="1">'
 for(let i=9;i>0;--i){
     str += '<tr>'
@@ -13,4 +13,4 @@ for(let i=9;i>0;--i){
 str +='</table>'
 document.getElementById('table2').innerHTML = str
 
-//倒九九乘法表 ， 请自行编写这段代码
+//倒九九乘法表 ， 请自行编写这段代
