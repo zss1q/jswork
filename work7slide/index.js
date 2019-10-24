1c99c262754421c5b37e2431aa3717e0279763e2
zss1q <1821493618@qq.com>
2019-10-24 09:35:57 +0800

    work7slide

--- work7slide/index.js
+++ work7slide/index.js
@@ -1,18 +1,14 @@
 function sort(method){
-    //读取待排序数据
     let origin = document.getElementById('origin').value
     let logs
     console.log(method)
-    //根据传入排序的方法调用不用的排序函数
     if(method=='bubble'){
-        //调用bubblesort()冒泡排序函数获取排序结果，
         logs = bubbleSort(origin)
     }else if(method=='insert'){
-        //调用insertSort()插入排序函数获取排序结果，
-        logs = insertSort(origin)    
+        logs = insertSort(origin)
     }
-    //获取显示冒泡排序的div
-    showDiv= document.getElementById('Anim')
-    //显示排序后的结果
-    showDiv.innerHTML = logs
+    showDiv = document.getElementById('Anim')
+    logValues = logs.values()
+    console.log(logValues.next())
+    setTimeout("showLog(logValues,showDiv)",50)
 }
--- work7slide/utils.js
+++ work7slide/utils.js
@@ -1,29 +1,65 @@
-//冒泡排序
-function bubbleSort(str) {
-    //把字符串切分成数组
+function bubbleSort(str){
     let arr = str.split(',')
-    //比较轮数，数组有多少个数字，就比较数组长度-1轮
-    for (let i = 0; i < arr.length - 1; i++){
-        //从第一个开始比较相邻的两个数值，
-        for (let j = 0; j < arr.length - i - 1; j++){
-            //比相邻的两个元素，如果左面的大，那么就把大的交换到右边
-            if (arr[j] > arr[j + 1]) {
-                //使用解构赋值大方法，把两个元素交换位置
-                [arr[j],arr[j + 1]] = [arr[j + 1], arr[j]]
+    let sortLog = []
+    for (let i = 0;i<arr.length - 1;i++){
+        for(let j = 0;j<arr.length - i - 1;j++){
+            sortLog.push([arr.concat(),[j, j + 1]])
+            sortLog.push([arr.concat(),[j, j + 1]])
+            sortLog.push([arr.concat(),[j, j + 1]])
+            if (arr[j]>arr[j + 1]){
+                [arr[j],arr[j + 1]] = [arr[j+1],arr[j]]
+                sortLog.push([arr.concat(),[j, j + 1]])
             }
         }
     }
-    return arr
+    sortLog.push([arr.concat(),[-1,-1]])
+    return sortLog
 }
-//插入排序 
+function showLog(logValues,showElement){
+    let str = ''
+    let{
+        done,
+        value: [row,pos]
+    } = logValues.next()
+    for(const key in row){
+        let color = pos.indexOf(Number(key)) > -1 ? 'color:red;' :''
+        str += '<span style="font-size:' + row[key] * 20 + 'px;' +
+        color + '">' + row[key] + '</span>'
+    }
+    showElement.innerHTML = str
+    if (pos[0] != -1)
+    setTimeout("showLog(logValues,showDiv)",500)
+}
 function insertSort(str) {
     let arr = str.split(',')
+    let sortLog = []
     for (let i = 1; i < arr.length; i++) {
         for (let j = i; j > 0; j--) {
+            sortLog.push([arr.concat(), [j, j - 1]])
+            sortLog.push([arr.concat(), [j, j - 1]])
+            sortLog.push([arr.concat(), [j, j - 1]])
             if (arr[j - 1] > arr[j]) {
                 [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]]
+                sortLog.push([arr.concat(), [j, j - 1]])
             }
         }
     }
-    return arr
+    sortLog.push([arr.concat(), [-1, -1]])
+    return sortLog
+}
+
+function showLog(logValues, showElement) {
+    let str = ''
+    let {
+        done,
+        value: [row, pos]
+    } = logValues.next()
+    for (const key in row) {
+        let color = pos.indexOf(Number(key)) > -1 ? 'color:red;' : ''
+        str += '<span style="font-size:' + row[key] * 20 +'px;' +
+                color + '">' + row[key] + '</span>'
+    }
+    showElement.innerHTML = str
+    if (pos[0] != -1)
+    setTimeout("showLog(logValues,showDiv)", 500)
 }
